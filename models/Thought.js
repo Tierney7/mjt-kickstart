43a4e34f3e91ef219690193f9deffd12ac916c8e
Matthew Tierney <tierneybbq@gmail.com>
2022-10-18 23:06:01 -0500

    backend/mini

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+const thoughtSchema = new Schema(
+  {
+    
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+        },
+   
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+    
+    reaction: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+const Thought = model('thought', thoughtSchema);
+
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const thoughtsSchema = require('./Thought');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, 
+      unique: true,
+      required: true,
+      max_length: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max_length: 50,
+    },
+   
+    thoughts: [thoughtsSchema],
+    
+
+    friends: [assignmentSchema],
+  },
+
+  
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
